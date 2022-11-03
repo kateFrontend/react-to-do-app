a193ff182e3ec9a1f24f7ef53e718d38b1f9256a
kateFrontend <ecdanilova@gmail.com>
2022-11-02 20:35:00 -0400

    update styles

--- src/Alert.js
+++ src/Alert.js
@@ -4,7 +4,7 @@ function Alert({type, msg, removeAlert, list}) { // everytime when the props are
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert()
-    },3000) // after 3 seconds invoke removeAlert and then hide alert
+    },2000) // after 2 seconds invoke removeAlert and then hide alert
     return () => clearTimeout(timeout)
   },[list])
   return <p className={`alert alert-${type}`}>{msg}</p>; // depending on the type it will be alert with the options danger or success
